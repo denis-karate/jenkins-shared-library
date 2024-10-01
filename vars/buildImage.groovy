c5743cd2158b887ddaece69bf7ea6884152b26bc
Denis <denis@example.com>
2024-10-01 18:22:00 +0300

    add vars files

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cr', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t denchikkarate/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push denchikkarate/demo-app:jma-2.0'
+    }
+} 
