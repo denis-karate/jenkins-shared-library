184c58052e8fc7d722f1959a08e903aa7f332c60
Denis <denis@example.com>
2024-10-07 18:15:22 +0300

    new version image 2.1

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cr', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t denchikkarate/demo-app:jma-2.0 .'
+        sh 'docker build -t denchikkarate/demo-app:jma-2.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push denchikkarate/demo-app:jma-2.0'
+        sh 'docker push denchikkarate/demo-app:jma-2.1'
     }
 } 
