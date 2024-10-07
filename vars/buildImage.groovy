cc6f16b48511b9ccc6b48c6943b50eb276c1d998
Denis <denis@example.com>
2024-10-07 18:48:09 +0300

    add string paremnt in buildImage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cr', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t denchikkarate/demo-app:jma-2.1 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push denchikkarate/demo-app:jma-2.1'
+        sh "docker push $imageName"
     }
 } 
